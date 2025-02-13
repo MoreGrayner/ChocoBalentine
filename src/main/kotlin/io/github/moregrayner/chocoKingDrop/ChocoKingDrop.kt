9d95d0a41a8915a2360ca35dfae4b801ad61bd3f
MoreGrayner <peargrayinyt@gmail.com>
2025-02-13 15:04:17 +0900

    Initial commit

---
+++ src/main/kotlin/io/github/moregrayner/chocoKingDrop/ChocoKingDrop.kt
@@ -0,0 +1,14 @@
+package io.github.moregrayner.chocoKingDrop
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class ChocoKingDrop : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
